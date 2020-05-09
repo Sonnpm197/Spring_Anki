c70204f056e700ca119a2d368afce4ec5d86712d
sonn197 <sonnmp197@gmail.com>
2020-05-09 23:32:02 +0700

    first commit

---
+++ src/main/java/com/son/anki/AnkiMain.java
@@ -0,0 +1,11 @@
+package com.son.anki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkiMain {
+    public static void main(String[] args) {
+        SpringApplication.run(AnkiMain.class, args);
+    }
+}
